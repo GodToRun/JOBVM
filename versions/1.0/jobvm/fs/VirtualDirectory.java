631e8387228051985113ea60b0cf43e8a5b9b325
GodToRun <74130463+GodToRun@users.noreply.github.com>
2021-07-10 17:26:09 +0900

    Add files via upload

---
+++ versions/1.0/jobvm/fs/VirtualDirectory.java
@@ -0,0 +1,40 @@
+package jobvm.fs;
+import java.util.*;
+public class VirtualDirectory{
+	public HashMap<String,VirtualFile> FileList = new HashMap<String,VirtualFile>();
+	public HashMap<String,VirtualDirectory> DirectoryList = new HashMap<String,VirtualDirectory>();
+	String Path;
+	public VirtualDirectory(VirtualDisk disk, String dirPath) {
+		Path = dirPath;
+		disk.AddDirectory(this);
+	}
+	public VirtualDirectory(VirtualDirectory dir, String dirPath) {
+		Path = dirPath;
+		dir.DirectoryList.put(this.Path,this);
+	}
+	public void AddFile(VirtualFile file) {
+		FileList.put(file.name,file);
+	}
+	public void DeleteFile(VirtualFile file) {
+		FileList.remove(file.name);
+	}
+	public void DeleteFile(String name) {
+		FileList.remove(name);
+	}
+	public void AddDirectory(VirtualDirectory dir) {
+		DirectoryList.put(dir.Path,dir);
+	}
+	public void DeleteDirectory(VirtualDirectory dir) {
+		DirectoryList.remove(dir.Path);
+	}
+	public void DeleteDirectory(String name) {
+		DirectoryList.remove(name);
+	}
+	public VirtualFile GetFile(String name) {
+		return this.FileList.get(name);
+	}
+	public VirtualDirectory GetDirectory(String name) {
+		return this.DirectoryList.get(name);
+	}
+
+}
